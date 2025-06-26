b8c3ca16c7d23670ecc5b1655485ba4bcd131ded
sahilchauhan0603 <sahilchauhan0603@gmail.com>
2025-06-26 23:28:07 +0530

    done

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -5,6 +5,8 @@ export const sendToken = (user, statusCode, res, message) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    sameSite: 'None', // Required for cross-origin cookies
+    secure: true      // Required for sameSite: 'None'
   };
 
   res.status(statusCode).cookie("token", token, options).json({
